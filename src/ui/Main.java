47375ea41f8c27acb9e113faa42508ddd2f0b3a4
Bruce <yichen89@msn.com>
2018-09-08 09:44:12 -0700

    P1 Get Started

---
+++ src/ui/Main.java
@@ -0,0 +1,20 @@
+package ui;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        printSample();
+        printSentence("I like cats.");
+    }
+
+    public static void printSample()
+    {
+        System.out.println("Hello, World");
+    }
+
+    public static void printSentence(String str)
+    {
+        System.out.println(str);
+    }
+}
