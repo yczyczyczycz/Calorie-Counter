0279f4a759bcd09c44c33645e1b88a2c3f0ef9c8
Bruce <yichen89@msn.com>
2018-11-24 18:22:48 -0800

    Project week 11 frame push

--- src/main/ui/CounterGUI.java
+++ src/main/ui/CounterGUI.java
@@ -2,10 +2,9 @@ package ui;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
-public class CounterGUI extends JFrame implements ActionListener{
+public class CounterGUI extends JFrame{
 
     private static final int FRAME_WIDTH = 600;
     private static final int FRAME_HEIGHT = 600;
@@ -31,7 +30,7 @@ public class CounterGUI extends JFrame implements ActionListener{
         JPanel panel = new JPanel();
         panel.setLayout(new BorderLayout());
         JPanel buttonPanel = new JPanel();
-        buttonPanel.setLayout(new GridLayout(2,3));
+        buttonPanel.setLayout(new GridLayout(3,2));
         panel.add(buttonPanel, BorderLayout.CENTER);
 
         //JLabel
@@ -42,14 +41,22 @@ public class CounterGUI extends JFrame implements ActionListener{
         //text
         text = new JTextField(20);
         panel.add(text, BorderLayout.SOUTH);
-
+        button6 = new JButton("enter");
+        button6.registerKeyboardAction(button6.getActionForKeyStroke(
+                KeyStroke.getKeyStroke(KeyEvent.VK_SPACE, 0, false)),
+                KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0, false),
+                JComponent.WHEN_FOCUSED);
+        button6.registerKeyboardAction(button6.getActionForKeyStroke(
+                KeyStroke.getKeyStroke(KeyEvent.VK_SPACE, 0, true)),
+                KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0, true),
+                JComponent.WHEN_FOCUSED);
         //buttons
-        button1 = new JButton("1");
-        button2 = new JButton("2");
-        button3 = new JButton("3");
-        button4 = new JButton("4");
-        button5 = new JButton("5");
-        button6 = new JButton("6");
+        button1 = new JButton("input meal");
+        button2 = new JButton("input workout");
+        button3 = new JButton("day calorie");
+        button4 = new JButton("activity list");
+        button5 = new JButton("daily calorie difference");
+
         buttonPanel.add(button1);
         buttonPanel.add(button2);
         buttonPanel.add(button3);
@@ -59,8 +66,6 @@ public class CounterGUI extends JFrame implements ActionListener{
 
         add(panel);
     }
-    @Override
-    public void actionPerformed(ActionEvent e) {
 
-    }
+
 }
